6d11cd387c993c993439ef983b2b0ebaea7b5cea
leechannl <leechannl@gmail.com>
2014-05-25 15:59:49 +0800

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by mm on 5/25/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
